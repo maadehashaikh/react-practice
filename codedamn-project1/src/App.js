f7e8654ae23bfa7396a2c0ee4bdf2d24abf6e551
Maadeha shaikh <maadeham@gmail.com>
2024-05-08 23:46:31 +0500

    first commit

---
+++ codedamn-project1/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import States from './States';
+function App() {
+  return (
+    <div className="App">
+      <h1>Its my react Project One </h1>
+      <States/>
+    </div>
+  );
+}
+
+export default App;
---
+++ codedamn-project1/src/States.jsx
@@ -0,0 +1,19 @@
+import React , {useState} from 'react'
+const States = () => {
+  const [count,setCount] = useState(0);
+  const increase = () => {
+    setCount(count+1);
+  }
+  const decrease = () => {
+    setCount(count-1);
+  }
+  return (
+    <>
+    <button onDoubleClick={increase}>Increase Count</button>
+    <button onClick={decrease}>Decrease Count</button>
+    <h1>Here is Count Number : {count}</h1>
+    </>
+  )
+}
+
+export default States
